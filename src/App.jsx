4906207353689d4b1bd1d219d6f41553686ca336
Bomin Zhang <davidzhangshs@gmail.com>
2024-11-15 16:57:15 -0500

    fixed UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
+import { Route, Routes, Link } from 'react-router-dom'
 import Registration from './components/Registration'
 import HostView from './components/HostView'
 
